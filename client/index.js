8a5e02214035c033bf0fa9b282a50b4af83aafa3
marilyncs <marilyn.n.chu@gmail.com>
2019-02-06 14:33:52 -0500

    emitting draws frm client

--- client/index.js
+++ client/index.js
@@ -4,11 +4,19 @@
 import whiteboard, {draw} from './whiteboard'
 
 whiteboard.on('draw', brushCoords => console.log('Brush is at x,y:', brushCoords))
+
+whiteboard.emit('draw', start, end, strokeColor);
+
 // Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true) //Q: why isn't this drawing a line?
+// draw([0, 0], [250, 250], 'red', true) // that's the annoying red line in the corner
 
 let socket = io(window.location.orgin)
 
 socket.on('connect', function(){
   console.log('I have made a persistent two-way connection to the server!')
 })
+
+socket.on('broadcast', function(broadcast){
+  console.log(broadcast);
+})
+
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const path = require('path')
 const express = require('express')
-const app = express()
+const app = express();
 const socketio = require('socket.io')
+const whiteboard = require('./client/whiteboard')
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
 const server = app.listen(1337, () => {
   console.log(`Listening on http://localhost:${server.address().port}`)
 })
+
 let io = socketio(server) //IN THE WORKSHOP THIS IS VAR NOT CONST/LET!
 
+let clients = 0;
 io.on('connection', function(socket){
+  clients ++;
   console.log('A new client has connected!!!')
   console.log(socket.id)
+
+  socket.on('disconnect', function() {
+    console.log('A client has disconnected :(');
+    console.log(socket.id)
+  })  
+  
+  io.emit('broadcast', `${clients} clients`)
+
+  // io.to(socket.id).emit('hellooô')
 })
+
+whiteboard.on('draw', (start, end, strokeColor) => {
+  console.log('listener recieved from clientside', start, end, strokeColor)
+});
 
 app.use(express.static(path.join(__dirname, 'public')))
 
